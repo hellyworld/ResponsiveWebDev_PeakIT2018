c025370c3b0adaa94a854634d4f250af18976c6e
Florin Asavei <asaveiflorin@gmail.com>
2018-11-27 20:13:15 +0200

    added favicon and footer component

--- project/script/app.js
+++ project/script/app.js
@@ -1,8 +1,12 @@
 document.addEventListener("DOMContentLoaded", function() {
     console.log('dom loaded');
     loadHeader();
+    loadFooter();
     document.addEventListener('headerLoaded', function (e) {
         console.log('header loaded');
+     }, false);
+     document.addEventListener('footerLoaded', function (e) {
+        console.log('footer loaded');
      }, false);
 
 });
--- project/script/loaders.js
+++ project/script/loaders.js
@@ -1,24 +1,34 @@
 function loadHeader() {
-    
-    fetch("/components/header.html")
-        .then(function(response) {
-          return response.text();
-        })
-        .then(function(markup) {
-          document.querySelector("#navigationContainer").innerHTML = markup;
-          var event = new Event('headerLoaded');
-          document.dispatchEvent(event);
-          
-          document
-            .querySelector("#expandNav")
-            .addEventListener("click", function() {
-              if (
-                document.querySelector("#navList").classList.contains("hidden")
-              ) {
-                document.querySelector("#navList").classList.remove("hidden");
-              } else {
-                document.querySelector("#navList").classList.add("hidden");
-              }
-            });
+  fetch("/components/header.html")
+    .then(function(response) {
+      return response.text();
+    })
+    .then(function(markup) {
+      document.querySelector("#navigationContainer").innerHTML = markup;
+      var event = new Event("headerLoaded");
+      document.dispatchEvent(event);
+
+      document
+        .querySelector("#expandNav")
+        .addEventListener("click", function() {
+          if (document.querySelector("#navList").classList.contains("hidden")) {
+            document.querySelector("#navList").classList.remove("hidden");
+          } else {
+            document.querySelector("#navList").classList.add("hidden");
+          }
         });
+    });
+}
+
+function loadFooter() {
+  fetch("/components/footer.html")
+    .then(function(response) {
+      return response.text();
+    })
+    .then(function(markup) {
+      document.querySelector("#footerContainer").innerHTML = markup;
+      var event = new Event("footerLoaded");
+      document.dispatchEvent(event);
+
+    });
 }
