9fa6a641379384b40666c79943275f24f7d1d993
florinasavei <asaveiflorin@gmail.com>
2018-11-29 19:31:00 +0200

    sync test script

--- utils/windows/git/resetTo_master.bat
+++ utils/windows/git/resetTo_master.bat
@@ -1,1 +1,1 @@
-git checkout master && git fetch && git clean -f && git clean -fd && git reset --hard HEAD  && git pull
+git fetch && git clean -f && git clean -fd && git reset --hard HEAD  && git pull && git checkout master
