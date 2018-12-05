277910d23e42972889809bea56fd6970f58a4bd6
Florin Asavei <asaveiflorin@gmail.com>
2018-12-05 22:14:53 +0200

    added the "filter mature jokes" functionality

--- project/script/app.js
+++ project/script/app.js
@@ -1,55 +1,72 @@
 document.addEventListener("DOMContentLoaded", function() {
-    console.log('DOM loaded');
-    loadHeader();
-    loadFooter();
+  console.log("DOM loaded");
+  loadHeader();
+  loadFooter();
 
-    document.addEventListener('headerLoaded', function (e) {
-        console.log('header loaded');
-     }, false);
-     document.addEventListener('footerLoaded', function (e) {
-        console.log('footer loaded');
-     }, false);
+  document.addEventListener(
+    "headerLoaded",
+    function(e) {
+      console.log("header loaded");
+    },
+    false
+  );
+  document.addEventListener(
+    "footerLoaded",
+    function(e) {
+      console.log("footer loaded");
+    },
+    false
+  );
 
-     //models
-     var getJokeButton = document.querySelector("#getJoke");
+  //models
+  var getJokeButton = document.querySelector("#getJoke");
 
-    //event listeners
-    getJokeButton.addEventListener("click", getJokeFromServer);
+  function chuckNorrisModel(fName, lName) {
+    this.firstName = fName || "Chuck";
+    this.lastName = lName || "Norris";
+  }
 
-    //todo: replace with AXIOS??
-    function getJokeFromServer(){
+  //event listeners
+  getJokeButton.addEventListener("click", getJokeFromServer);
 
-      var firstNameVal = document.querySelector('#firstName').value;
-      var lastNameVal = document.querySelector('#lastName').value;
+  function getJokeFromServer() {
+    var firstNameVal = document.querySelector("#firstName").value;
+    var lastNameVal = document.querySelector("#lastName").value;
 
-      var url = new URL('http://api.icndb.com/jokes/random/');
+    var isSafeForWork =  document.querySelector("#filderMature").checked;
 
-      console.dir(url);
+    var myChuckNorris = new chuckNorrisModel(firstNameVal, lastNameVal);
 
-      var exludedCathegories = ["explicit"] ;
+    var url = new URL("http://api.icndb.com/jokes/random/");
 
-      var params = {exclude: '"["explicit"]"' , firstName : firstNameVal, lastName: lastNameVal}
+    console.dir(url);
 
-      url.search = new URLSearchParams(params)
+    var exludedCathegories = isSafeForWork ? "['explicit']" : [];
+    
+    var params = {
+      firstName: myChuckNorris.firstName,
+      lastName: myChuckNorris.lastName,
+      exclude: exludedCathegories
+      //exclude: '[excplicit]'
+    };
 
-      fetch(url)
+    url.search = new URLSearchParams(params);
+
+    fetch(url)
       .then(function(response) {
-         console.log('initial response: ', response);
-         return response.json();
+        console.log("initial response: ", response);
+        return response.json();
       })
       .then(function(jsonResponse) {
-         console.log('json response: ', jsonResponse);
-         displayJoke(jsonResponse);
+        console.log("json response: ", jsonResponse);
+        displayJoke(jsonResponse);
       });
-   }
+  }
 
-   function displayJoke (joke){
-      //console.log('doc container',  document.querySelector('#jokeContainer p'));
-      document.querySelector('#jokeContainer p').innerHTML = joke.value.joke;
-   }
-
+  function displayJoke(joke) {
+    //console.log('doc container',  document.querySelector('#jokeContainer p'));
+    document.querySelector("#jokeContainer p").innerHTML = joke.value.joke;
+  }
 });
 
-function arrayToString(myAray){
-
-}
+function arrayToString(myAray) {}
